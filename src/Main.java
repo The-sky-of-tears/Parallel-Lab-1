94c6c367d6f792e34d120c87c14f3d80bb0c89b8
Artem Skuratovskyi <theskyoftears13@gmail.com>
2023-02-19 21:46:22 +0200

    typo

--- src/Main.java
+++ src/Main.java
@@ -7,7 +7,6 @@ public class Main {
 		//the first iteration takes up to 10 times longer,
 		// I suppose the reason is init of JVM, so I ignore it for more accurate testing
 		new Runner().run(10000, 4);
-		new Runner().run(10000, 4);
 
 		for (int i = 0; i < 10; i++) {
 			double temp = new Runner().run(10_000, 256);
